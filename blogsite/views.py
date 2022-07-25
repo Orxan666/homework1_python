952e0508da6ef0f483794a603d4399b0cf5e1f27
orxan666 <orxanallahyarov03@gmail.com>
2022-07-25 15:39:12 +0400

    25.07.2022

---
+++ blogsite/views.py
@@ -0,0 +1,18 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def post(request):
+    return render(request, "post.html")
